cf7102e2cf805b53f897fc8ff86434b5c66beb3c
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-13 13:06:54 +0800

    feat : add condition style in table

--- training2_looping.php
+++ training2_looping.php
@@ -26,11 +26,20 @@ do {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Looping</title>
+    <style>
+        .lineColor{
+            background-color: green;
+        }
+    </style>
 </head>
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
         <?php for ($i=1; $i <= 5; $i++) : ?>
+            <?php if($i % 2 == 1) : ?>
+            <tr class="lineColor">
+            <?php else: ?>
             <tr>
+            <?php endif; ?>
                 <?php for ($j=1; $j <= 5; $j++) : ?>
                     <td><?= "$i-$j" ?></td>
                 <?php endfor; ?>
