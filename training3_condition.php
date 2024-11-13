11a7babc74bd713aa18388b6219d982b6dbf5288
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-13 13:06:27 +0800

    new : learn condition

---
+++ training3_condition.php
@@ -0,0 +1,20 @@
+<?php
+// Condition
+// if else
+// $x = 10;
+// if ($x < 10) {
+//     echo "True!";
+// } else {
+//     echo "Wrong!";
+// }
+
+// if else if else
+// $y = 10;
+// if ($y < 10) {
+//     echo "True!";
+// } else if($y == 10){
+//     echo "Equal!";
+// } else {
+//     echo "Wrong!";
+// }
+?>
