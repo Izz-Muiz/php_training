28fbac29f2c0bf05997709428352d2cfd5daccd1
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-13 17:46:05 +0800

    new : use student to learn

--- training5_1_array.php
+++ training5_1_array.php
@@ -1,5 +1,17 @@
-<?php
-$students = ["Ali", 20, "IT"];
+<?php 
+$students = [
+    [
+        "name" => "Muiz",
+        "age" => 20,
+        "city" => "Terengganu",
+        "picture" => "next.png"
+    ],
+    [
+        "name" => "Muaz",
+        "age" => 21,
+        "city" => "Johor"
+    ],
+];
 ?>
 
 <!DOCTYPE html>
@@ -7,14 +19,17 @@ $students = ["Ali", 20, "IT"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Register</h1>
-    <ul>
-        <?php foreach  ($students as $student) { ?>
-            <li><?= $student ?></li>
-        <?php } ?>
-    </ul>
+    <h1>Students</h1>
+    <?php foreach ($students as $student) { ?>
+        <ul>
+            <li><img src="<?= $student["picture"]; ?>" alt=""></li>
+            <li>Name : <?= $student["name"] ?></li>
+            <li>Age : <?= $student["age"] ?></li>
+            <li>City : <?= $student["city"] ?></li>
+        </ul>
+    <?php }?>
 </body>
 </html>
