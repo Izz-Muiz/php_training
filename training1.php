635b8a6bbc5512fad29951a22a28ab1978883fc5
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-13 11:53:53 +0800

    new : learn php syntax

---
+++ training1.php
@@ -0,0 +1,33 @@
+<?php
+// PHP Sintax
+
+// Standard Output
+// echo, print
+// print_r() - for debug
+// var_dump() - for debug
+
+// Variable
+// $name = "Ameera";
+
+// Operator
+// + - * / %
+
+// Concat or merge
+// .
+// $first_name = "Ahmad";
+// $last_name = "Abu";
+// echo $first_name . " " . $last_name;
+
+// Assignment
+// += -= *= /= %= .=
+
+// Differentiate
+// < > <= >= == !=
+
+// Identity
+// === !==
+
+// Logic
+// && || !
+ 
+?>
