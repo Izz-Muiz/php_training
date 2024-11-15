9d1de68b5f0c330b701c488e62a4018bbdaa3441
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-15 09:07:59 +0800

    new : add live searching using ajax

--- spp/index.php
+++ spp/index.php
@@ -60,43 +60,47 @@ if (isset($_POST["find"])) {
                 <?php if($activePage < $totalPage) : ?>
                     <a href="?page=<?= $activePage+1 ?>">></a>
                     <?php endif; ?>
-                    <table border="1" cellpadding="10" cellspacing="0">
-                        <thead>
-                <tr> 
-                    <th>No.</th>
-                    <th>Action</th>
-                    <th>Image</th>
-                    <th>IC</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Course</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 1; ?>
-                <?php foreach ($students as $student): ?>
-                    <tr>
-                        <td><?= $i; ?></td>
-                        <td>
-                            <a href="update.php?id=<?= $student['id']; ?>">Update</a>
-                            <a href="delete.php?id=<?= $student['id']; ?>" onclick="return confirm('Are you sure you want to delete this student?');">Delete</a>
-                        </td>
-                        <td>
-                            <img src="img/<?= htmlspecialchars($student['image']); ?>" alt="Image of <?= htmlspecialchars($student['name']); ?>" style="width: 50px; height: auto;">
-                        </td>
-                        <td><?= htmlspecialchars($student['ic']); ?></td>
-                        <td><?= htmlspecialchars($student['name']); ?></td>
-                        <td><?= htmlspecialchars($student['email']); ?></td>
-                        <td><?= htmlspecialchars($student['course']); ?></td>
+                    <div id="container">
+
+                            <table border="1" cellpadding="10" cellspacing="0">
+                                <thead>
+                                    <tr> 
+                                        <th>No.</th>
+                        <th>Action</th>
+                        <th>Image</th>
+                        <th>IC</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Course</th>
                     </tr>
-                    <?php $i++; ?>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($students as $student): ?>
+                        <tr>
+                            <td><?= $i; ?></td>
+                            <td>
+                                <a href="update.php?id=<?= $student['id']; ?>">Update</a>
+                                <a href="delete.php?id=<?= $student['id']; ?>" onclick="return confirm('Are you sure you want to delete this student?');">Delete</a>
+                            </td>
+                            <td>
+                                <img src="img/<?= htmlspecialchars($student['image']); ?>" alt="Image of <?= htmlspecialchars($student['name']); ?>" style="width: 50px; height: auto;">
+                            </td>
+                            <td><?= htmlspecialchars($student['ic']); ?></td>
+                            <td><?= htmlspecialchars($student['name']); ?></td>
+                            <td><?= htmlspecialchars($student['email']); ?></td>
+                            <td><?= htmlspecialchars($student['course']); ?></td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+    </div>
     </main>
 
     <footer>
         <p>&copy; <?= date("Y"); ?> Student Registration System</p>
     </footer>
+    <script src="js/script.js"></script>
 </body>
 </html>
