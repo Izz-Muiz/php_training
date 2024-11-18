23b4b9df9e6c12c577dce34c17618f7d10b7dea0
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-18 17:08:04 +0800

    Remove sensitive file from history

--- spp/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database connection parameters
-return [
-    'host' => 'sql300.infinityfree.com',
-    'username' => 'if0_37717443',
-    'password' => 'Fy8m4KyTG0pPqqz',
-    'database' => 'if0_37717443_spp',
-];
